91174eeb8b2f6c132c488d5f20bb88b248d0d902
rishavbhurtel <rishavmani.bhurtel@gmail.com>
2020-08-15 13:32:37 +0545

    updated

--- scrapy/trustpilot/spiders/scraper.py
+++ scrapy/trustpilot/spiders/scraper.py
@@ -9,9 +9,9 @@ class Pages(scrapy.Spider):
     start_urls = company_data['company_url'].unique().tolist()
 
     def parse(self, response):
-        company_logo = response.xpath('//img[@class="business-unit-profile-summary__image"]/@src').extract_first()
-        company_website = response.xpath("//a[@class='badge-card__section badge-card__section--hoverable company_website']/@title").extract_first()
-        company_name = response.xpath("//span[@class='multi-size-header__big']/text()").extract_first()
+     	# company_logo = response.xpath('//img[@class="business-unit-profile-summary__image"]/@src').extract_first()
+        # company_website = response.xpath("//a[@class='badge-card__section badge-card__section--hoverable company_website']/@title").extract_first()
+        # company_name = response.xpath("//span[@class='multi-size-header__big']/text()").extract_first()
         comments = response.xpath("//p[@class='review-content__text']")
         comments = [comment.xpath('.//text()').extract() for comment in comments]
         comments = [[c.strip() for c in comment_list] for comment_list in comments]
@@ -24,10 +24,10 @@ class Pages(scrapy.Spider):
             yield {
                 'comment': comment,
                 'rating': rating,
-                'url_website' : response.url,
-                'company_name': company_name,
-                'company_website': company_website,
-                'company_logo': company_logo
+                # 'url_website' : response.url,
+                # 'company_name': company_name,
+                # 'company_website': company_website,
+                # 'company_logo': company_logo
             }
 
         next_page = response.css('a[data-page-number=next-page] ::attr(href)').extract_first()
